5282699826031d3d8fe7a3ce9e01a9d5a66d8de6
p-schaefer <patrick.schaefer@mail.utoronto.ca>
2022-10-01 22:18:22 -0400

    fix

--- R/attrib_points.R
+++ R/attrib_points.R
@@ -562,10 +562,10 @@ attrib_points<-function(
     mutate(distance_weights=map(attrib,~.$distance_weights)) %>%
     mutate(weighted_attr=map(attrib,~.$weighted_attr)) %>%
     unnest(attrib_out) %>%
-    select(any_of(site_id_col),distance_weights,weighted_attr,everything(),-attrib) %>%
+    select(any_of(site_id_col),any_of("distance_weights"),any_of("weighted_attr"),everything(),-attrib) %>%
     mutate(across(c(everything(),-any_of(site_id_col),-any_of("distance_weights"),-any_of("weighted_attr")),as.numeric)) %>%
-    mutate(across(ends_with("_prop"),~case_when(is.na(.) | is.nan(.) ~ 0, T ~ .))) %>%
-    distinct()
+    mutate(across(ends_with("_prop"),~case_when(is.na(.) | is.nan(.) ~ 0, T ~ .))) #%>%
+    #dplyr::distinct()
 
 
   zip_loc<-input$outfile
