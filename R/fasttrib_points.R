f6832083d9c95f1353e7920c0ac4572b05d4f65f
p-schaefer <patrick.schaefer@mail.utoronto.ca>
2022-10-28 14:37:16 -0400

    .

--- R/fasttrib_points.R
+++ R/fasttrib_points.R
@@ -1809,7 +1809,7 @@ parallel_layer_processing <- function(n_cores,
   total_procs<-0
 
 
-  progressr::with_progress(enable=progressr::progress,{
+  progressr::with_progress(enable=T,{
     p <- progressr::progressor(steps = total_outs)
 
 
